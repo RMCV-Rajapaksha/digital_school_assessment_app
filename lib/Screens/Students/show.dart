f1f6b90acf8411d6117ac189b73105e4326f05b3
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-05-19 21:37:07 +0530

    chore: Initial commit

--- lib/Screens/Students/show.dart
+++ lib/Screens/Students/show.dart
@@ -38,7 +38,7 @@ class StuShowData extends StatelessWidget {
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-                        hintText: 'Search',
+                        hintText: 'Search using Reg No',
                         hintStyle: const TextStyle(
                           color: Color.fromRGBO(255, 255, 255, 1),
                           fontSize: 15,
@@ -90,15 +90,221 @@ class StuShowData extends StatelessWidget {
                 height: screenHeight * 0.02,
               ),
               Container(
+                width: screenWidth * 0.9,
                 padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
                 decoration: BoxDecoration(
                   color: const Color.fromRGBO(255, 255, 255, 0.1),
                   borderRadius: BorderRadius.circular(20.0),
                 ),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [],
+                  children: [
+                    SizedBox(
+                      height: screenHeight * 0.02,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft, // Align text to the left
+                      child: RichText(
+                        text: const TextSpan(
+                          text: 'Name : - ', // Default style
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18), // White text color
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'R.M.C.V Rajapaksha',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white), // White text color
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.005,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: RichText(
+                        text: const TextSpan(
+                          text: 'Reg No : - ',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'EG/2021/4733',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.005,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: RichText(
+                        text: const TextSpan(
+                          text: 'Mobile No : - ',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: '0779121861',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.01,
+                    ),
+                    DataTable(
+                      columns: const <DataColumn>[
+                        DataColumn(
+                          label: Text(
+                            'Semester ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'GPA',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ],
+                      rows: const <DataRow>[
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 1',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 2',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.7',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 3',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 4',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 6',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 7',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Semester 8',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(
+                              'Average GPA',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            )),
+                            DataCell(Text(
+                              '3.5',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ],
+                        ),
+                        // Add more DataRows here
+                      ],
+                    ),
+                  ],
                 ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
               ),
             ],
           ),
